5d090a19ac7dde726c7198ed6838ca2c204784dc
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-18 13:40:26 +0300

    Fill and print double array

---
+++ zadacha_49/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+double[,] FillArray(int m, int n)
+{
+    double[,] someMatrix = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        someMatrix[i,j] = (new Random()).Next(-20,80)/10.0;
+    }
+    return someMatrix;
+}
+void PrintArray(double [,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write($"Введите количество строк m: ");
+int m = int.Parse(Console.ReadLine() ??"0");
+Console.Write($"Введите количество столбцов n: ");
+int n = int.Parse(Console.ReadLine() ??"0");
+double[,] matrix = FillArray(m,n);
+PrintArray(matrix);
