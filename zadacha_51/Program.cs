6f6bd5783a46077ea031e3d7650d6bfcba3d44b8
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-18 14:42:37 +0300

    A[i,j] = i+j

---
+++ zadacha_51/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+int[,] FillArray(int rows, int columns)
+{
+    int[,] newMatrix = new int[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newMatrix[i,j] = i + j;
+        }
+    }
+    return newMatrix;
+}
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write($"Введите количество строк m: ");
+int m = int.Parse(Console.ReadLine() ??"0");
+Console.Write($"Введите количество столбцов n: ");
+int n = int.Parse(Console.ReadLine() ??"0");
+int[,] matrix = FillArray(m, n);
+PrintArray(matrix);
+
+
